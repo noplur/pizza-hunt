16563ec78bf48f7c0230ac6aef2af1aa1516772b
noplur <geoduckphoto@gmail.com>
2021-04-07 22:00:46 -0700

    reply to comments capability

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
-
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
-  } = require('../../controllers/pizza-controller');
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
-// Set up GET all and POST at /api/pizzas
+// /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
 
 // /api/pizzas/:id
 router
@@ -16,12 +19,5 @@ router
   .get(getPizzaById)
   .put(updatePizza)
   .delete(deletePizza);
-
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
-router
-  .route('/:id')
-  .get()
-  .put()
-  .delete();
 
 module.exports = router;
